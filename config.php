9736e2c111fba78f10ce0c7ff46351f96ec6ad80
romanthomas <roman@kirstein.de>
2014-02-14 21:05:13 +0100

    Um echo erweitert

--- config.php
+++ config.php
@@ -2,4 +2,6 @@
 // Testanwendung Konfiguration
 $test = "Hallo schöne neue Welt";
 $test2 = "Aber hallo - das sollten wir schon auch noch dazufügen!!!"
+
+echo $test . " online ergänzt! SUUUUUPER :)"
 ?>
